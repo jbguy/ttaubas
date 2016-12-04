c697ef427347f16871f5414dd399a4eb92623489
JB <guy.jbaptiste@gmail.com>
2016-12-04 23:13:22 +0100

    home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,9 +7,22 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  icons: string[];
+  articles: Array<{title: string, text: string, icon: string}>;
   constructor(public navCtrl: NavController) {
 
+    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
+    'american-football', 'boat', 'bluetooth', 'build'];
+
+    this.articles = [];
+    
+    for(let i = 1; i < 11; i++) {
+      this.articles.push({
+        title: 'Item ' + i,
+        text: 'This is item #' + i,
+        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+      });
+    }
   }
 
 }
